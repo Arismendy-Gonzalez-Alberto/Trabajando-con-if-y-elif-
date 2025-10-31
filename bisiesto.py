939b196ab9a46e1a08fdfecce1650ee3fde88195
Arismendy <2025-0296@unad.edu.do>
2025-10-30 21:05:57 -0400

    tarea sobre if y elif

--- bisiesto.py
+++ bisiesto.py
@@ -1,7 +1,7 @@
 a = int(input("Ingrese un año: "))
 
 if a < 0:
-    print("El Año invalido")
+    print(f"El Año {a} invalido")
     print("Tiene que ser un número positivo")
     
 elif a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
